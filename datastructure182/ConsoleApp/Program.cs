36859fe35a7a27495c6496758c6e2504a547cb9d
sharpencil改二 <yukinyaa712@gmail.com>
2018-09-25 12:58:17 +0900

    Pharsing

---
+++ datastructure182/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var csv = new TrainLoadingDataCSV();
+
+    }
+}
